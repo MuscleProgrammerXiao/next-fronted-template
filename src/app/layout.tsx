f5b7d7bc3ad7e1125f1bd277fef40e9e0f69615a
Cerberus <895422334@qq.com>
2024-10-15 22:49:12 +0800

    xx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,31 @@
+'use client';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import BasicLayout from '@/layouts/BasicLayout';
-import '../assets/globals.scss';
+import React, { useCallback, useEffect } from 'react';
+import '../assets/globals.css';
+
+/**
+ * 全局初始化函数
+ * @param children
+ * @returns
+ */
+const InitLayout: React.FC<
+	Readonly<{
+		children: React.ReactNode;
+	}>
+> = ({ children }) => {
+	/**
+	 * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+	 */
+	const doInit = useCallback(() => {
+		console.log('欢迎来到我的项目！');
+	}, []);
+	useEffect(() => {
+		doInit();
+	}, []);
+	return children;
+};
+
 export default function RootLayout({
 	children,
 }: Readonly<{
@@ -10,7 +35,9 @@ export default function RootLayout({
 		<html lang="zh">
 			<body>
 				<AntdRegistry>
-					<BasicLayout>{children}</BasicLayout>
+					<InitLayout>
+						<BasicLayout>{children}</BasicLayout>
+					</InitLayout>
 				</AntdRegistry>
 			</body>
 		</html>
