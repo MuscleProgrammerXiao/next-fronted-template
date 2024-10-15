627b62ac4a7a6c0b1a887033881371375a2d7998
superflashcx <895422334@qq.com>
2024-10-15 20:28:20 +0800

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,26 @@ import style from './page.module.scss';
 export default function Page() {
 	return (
 		<main className={style.page}>
-			<header>header</header>
+			<header
+				style={{
+					display: 'flex',
+					justifyContent: 'space-between',
+					alignItems: 'center',
+					paddingLeft: '28px',
+					textAlign: 'center',
+					height: '30px',
+					backgroundColor: 'gray',
+				}}
+			>
+				<p>logo</p>
+				<nav>
+					<div style={{ display: 'flex', columnGap: '20px', fontSize: '16px' }}>
+						<span>link1</span>
+						<span>link2</span>
+						<span>link3</span>
+					</div>
+				</nav>
+			</header>
 			<main>main</main>
 			<footer>footer</footer>
 		</main>
